7c228f7cd0323b43c714f85ca514dcc1f82df3f7
Fuz7 <124787267+Fuz7@users.noreply.github.com>
2023-08-15 19:55:53 +0800

    Finish Overall Layout

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import './static/styles.css'
+import './static/styles.css';
 
 const toggle = document.getElementById('toggleButton');
 
-toggle.addEventListener('click',()=>{
-  toggle.classList.toggle('toggled')
-}
-)
+toggle.addEventListener('click', () => {
+  toggle.classList.toggle('toggled');
+});
